fd07b589d16701420e5b2d80578b3a3c5a8456a3
Mohmmad Shah Faishal <faishal@scrambleapps.com>
2022-05-04 16:34:31 +0530

    Initial Commit

---
+++ Jungle Safari/Jungle_SafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jungle_SafariApp.swift
+//  Jungle Safari
+//
+//  Created by faishal on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Jungle_SafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
